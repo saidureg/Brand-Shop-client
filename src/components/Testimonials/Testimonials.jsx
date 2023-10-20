a3d6c7cea2f3a8eade85454b5aa279b2bb3255cf
saidureg <saidureg@gmail.com>
2023-10-20 18:54:57 +0600

    client testimonials section is add in the home page

---
+++ src/components/Testimonials/Testimonials.jsx
@@ -0,0 +1,291 @@
+const Testimonials = () => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 mx-3 lg:mx-0">
+      <div className="relative flex w-full max-w-[26rem] flex-col rounded-xl bg-transparent bg-clip-border text-gray-700 shadow-none border p-3">
+        <div className="relative flex items-center gap-4 pt-0 pb-8 mx-0 mt-4 overflow-hidden text-gray-700 bg-transparent shadow-none rounded-xl bg-clip-border">
+          <img
+            src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1480&amp;q=80"
+            alt="Michael Rodriguez"
+            className="relative inline-block h-[58px] w-[58px] !rounded-full object-cover object-center"
+          />
+          <div className="flex w-full flex-col gap-0.5">
+            <div className="flex items-center justify-between">
+              <h5 className="block font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-gray-900">
+                Michael Rodriguez
+              </h5>
+              <div className="flex items-center gap-0 5">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  aria-hidden="true"
+                  className="w-5 h-5 text-yellow-700"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  aria-hidden="true"
+                  className="w-5 h-5 text-yellow-700"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  aria-hidden="true"
+                  className="w-5 h-5 text-yellow-700"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  aria-hidden="true"
+                  className="w-5 h-5 text-yellow-700"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  aria-hidden="true"
+                  className="w-5 h-5 text-yellow-700"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              </div>
+            </div>
+            <p className="block font-sans text-base antialiased font-light leading-relaxed text-gray-900">
+              Tech Innovators Inc.
+            </p>
+          </div>
+        </div>
+        <div className="p-0 mb-6">
+          <p className="block font-sans text-base antialiased font-light leading-relaxed text-inherit">
+            NexGen exceeded our expectations in every way. From laptops to
+            smartphones, every item we purchased was of the highest quality. The
+            NexGen teams expertise and responsiveness made the entire process
+            seamless.!!!
+          </p>
+        </div>
+      </div>
+      <div className="relative flex w-full max-w-[26rem] flex-col rounded-xl bg-transparent bg-clip-border text-gray-700 shadow-none border p-3">
+        <div className="relative flex items-center gap-4 pt-0 pb-8 mx-0 mt-4 overflow-hidden text-gray-700 bg-transparent shadow-none rounded-xl bg-clip-border">
+          <img
+            src="https://i.ibb.co/zHFMmKS/team-member-1.png"
+            alt="tania andrew"
+            className="relative inline-block h-[58px] w-[58px] !rounded-full object-cover object-center"
+          />
+          <div className="flex w-full flex-col gap-0.5">
+            <div className="flex items-center justify-between">
+              <h5 className="block font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-gray-900">
+                Tania Andrew
+              </h5>
+              <div className="flex items-center gap-0 5">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  aria-hidden="true"
+                  className="w-5 h-5 text-yellow-700"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  aria-hidden="true"
+                  className="w-5 h-5 text-yellow-700"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  aria-hidden="true"
+                  className="w-5 h-5 text-yellow-700"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  aria-hidden="true"
+                  className="w-5 h-5 text-yellow-700"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  aria-hidden="true"
+                  className="w-5 h-5 text-yellow-700"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              </div>
+            </div>
+            <p className="block font-sans text-base antialiased font-light leading-relaxed text-gray-900">
+              White & Co. Design
+            </p>
+          </div>
+        </div>
+        <div className="p-0 mb-6">
+          <p className="block font-sans text-base antialiased font-light leading-relaxed text-inherit">
+            Working with NexGen has been an absolute pleasure. Their innovative
+            products and exceptional customer service have transformed our
+            offices technology infrastructure. The team is highly professional,
+            attentive, and always goes the extra mile to ensure our needs are
+            met.!!!
+          </p>
+        </div>
+      </div>
+      <div className="relative flex w-full max-w-[26rem] flex-col rounded-xl bg-transparent bg-clip-border text-gray-700 shadow-none border p-3">
+        <div className="relative flex items-center gap-4 pt-0 pb-8 mx-0 mt-4 overflow-hidden text-gray-700 bg-transparent shadow-none rounded-xl bg-clip-border">
+          <img
+            src="https://i.ibb.co/Pw5FDZR/team-member3.png"
+            alt="David Lee"
+            className="relative inline-block h-[58px] w-[58px] !rounded-full object-cover object-center"
+          />
+          <div className="flex w-full flex-col gap-0.5">
+            <div className="flex items-center justify-between">
+              <h5 className="block font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-gray-900">
+                David Lee
+              </h5>
+              <div className="flex items-center gap-0 5">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  aria-hidden="true"
+                  className="w-5 h-5 text-yellow-700"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  aria-hidden="true"
+                  className="w-5 h-5 text-yellow-700"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  aria-hidden="true"
+                  className="w-5 h-5 text-yellow-700"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  aria-hidden="true"
+                  className="w-5 h-5 text-yellow-700"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  aria-hidden="true"
+                  className="w-5 h-5 text-yellow-700"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              </div>
+            </div>
+            <p className="block font-sans text-base antialiased font-light leading-relaxed text-gray-900">
+              Lee Enterprises
+            </p>
+          </div>
+        </div>
+        <div className="p-0 mb-6">
+          <p className="block font-sans text-base antialiased font-light leading-relaxed text-inherit">
+            NexGen truly understands the needs of modern businesses. Their range
+            of electronics products, from smartphones to smart home devices,
+            caters to every requirement. The reliability and efficiency of their
+            products have streamlined our operations. !!!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Testimonials;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,8 @@ import { useLoaderData } from "react-router-dom";
 import Brands from "../../components/Brands/Brands";
 import Banner from "../../components/Header/Banner";
 import Navbar from "../../components/Header/Navbar";
+import FeaturedProducts from "../../components/FeaturedProducts/FeaturedProducts";
+import Testimonials from "../../components/Testimonials/Testimonials";
 
 const Home = () => {
   const brands = useLoaderData();
@@ -28,61 +30,21 @@ const Home = () => {
         <h3 className="text-[#E2DAA7] text-center text-5xl my-10">
           Featured Products
         </h3>
-
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3 mx-3 lg:mx-0">
-          <div className="card bg-base-100 shadow-xl mx-6 md:mr-3 lg:mx-0">
-            <figure className="px-6 pt-5">
-              <img
-                src="https://i.ibb.co/R2LVsMF/i-Phone-14-Pro.png"
-                alt="i-Phone-14-Pro"
-                className="rounded-xl w-4/6 h-[250px]"
-              />
-            </figure>
-            <div className="card-body items-center text-center">
-              <h2 className="card-title">iPhone 14 Pro Max</h2>
-              <p className="text-gray-500 font-bold text-lg">$ 799.00</p>
-            </div>
-          </div>
-          <div className="card bg-base-100 shadow-xl mx-6 md:mr-3 lg:mx-0">
-            <figure className="px-6 pt-5">
-              <img
-                src="https://i.ibb.co/6sktBfP/One-Plus-10-Pro.png"
-                alt="i-Phone-14-Pro"
-                className="rounded-xl w-4/6 h-[250px]"
-              />
-            </figure>
-            <div className="card-body items-center text-center">
-              <h2 className="card-title">OnePlus 10 Pro</h2>
-              <p className="text-gray-500 font-bold text-lg">$ 799.00</p>
-            </div>
-          </div>
-          <div className="card bg-base-100 shadow-xl mx-6 md:mr-3 lg:mx-0">
-            <figure className="px-6 pt-5">
-              <img
-                src="https://i.ibb.co/CW9ZVP5/Vivo-X60-Pro.png"
-                alt="i-Phone-14-Pro"
-                className="rounded-xl w-4/6 h-[250px]"
-              />
-            </figure>
-            <div className="card-body items-center text-center">
-              <h2 className="card-title">Vivo X60 Pro+</h2>
-              <p className="text-gray-500 font-bold text-lg">$ 399.00</p>
-            </div>
-          </div>
-          <div className="card bg-base-100 shadow-xl mx-6 md:mr-3 lg:mx-0">
-            <figure className="px-6 pt-5">
-              <img
-                src="https://i.ibb.co/7GfxKgg/Dell-XPS-13.png"
-                alt="i-Phone-14-Pro"
-                className="rounded-xl w-4/6 h-[250px]"
-              />
-            </figure>
-            <div className="card-body items-center text-center">
-              <h2 className="card-title">Dell XPS 13</h2>
-              <p className="text-gray-500 font-bold text-lg">$ 1299.00</p>
-            </div>
-          </div>
-        </div>
+        <FeaturedProducts />
+      </div>
+      <div className="max-w-screen-xl mx-auto mb-10">
+        <h6 className="text-gray-400 text-center text-sm mt-10">
+          Reviewed by client
+        </h6>
+        <h3 className="text-gray-800 text-center text-lg lg:text-3xl my-5 mx-3 md:mx-0">
+          Discover What Our Clients Say About Us
+        </h3>
+        <p className="text-gray-600 mb-4 text-center w-3/4 mx-auto">
+          Heres what our valued clients have to say about their experiences with
+          us. Their words reflect the dedication and expertise we bring to every
+          project, ensuring your satisfaction and success.
+        </p>
+        <Testimonials />
       </div>
     </div>
   );
